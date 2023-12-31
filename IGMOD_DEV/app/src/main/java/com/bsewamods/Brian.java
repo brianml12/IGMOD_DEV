afacf291475bf658e34ccc5719603dc5a408b2c1
brianml12 <s20120168@alumnos.itsur.edu.mx>
2023-12-30 22:03:19 -0600

    Mejoras menores en el codigo

--- IGMOD_DEV/app/src/main/java/com/bsewamods/Brian.java
+++ IGMOD_DEV/app/src/main/java/com/bsewamods/Brian.java
@@ -1,7 +1,9 @@
 package com.bsewamods;
 
 import android.app.Activity;
+import android.app.AlarmManager;
 import android.app.AlertDialog;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -19,18 +21,7 @@ import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Brian implements View.OnLongClickListener {
-    private final InstagramMainActivity mainActivity;
-
-    public Brian(InstagramMainActivity mainActivity) {
-        this.mainActivity = mainActivity;
-    }
-
-    public static void onlongClickMenu(InstagramMainActivity activity, View v){
-        Brian objBrian = new Brian(activity);
-        v.setOnLongClickListener(objBrian);
-    }
-
+public class Brian {
     static void showToast(Context ctx, String textoToast, int duracion){
         Toast.makeText(ctx, textoToast, duracion).show();
     }
@@ -69,6 +60,7 @@ public class Brian implements View.OnLongClickListener {
                 showToast(ctx,"Imported file", Toast.LENGTH_LONG);
                 FID.close();
                 FOS.close();
+                showDialogRestartApp(ctx);
             }
         }catch (Exception e){
             showToast(ctx,"Error: Could not import developer mode settings",Toast.LENGTH_LONG);
@@ -81,64 +73,95 @@ public class Brian implements View.OnLongClickListener {
         ctx.startActivity(intent);
     }
 
-    @Override
-    public boolean onLongClick(View v) {
-        String[] strListaMenu = {"Open Developer Menu", "Create developer mode backup", "Restore developer mode backup"};
-        AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
-        builder.setItems(strListaMenu, new DialogInterface.OnClickListener() {
+    static void showDialogRestartApp(Context ctx){
+        AlertDialog.Builder alertBuilderD = new AlertDialog.Builder(ctx);
+        alertBuilderD.setTitle("RESTART APP");
+        alertBuilderD.setMessage("To apply the changes it is necessary to restart the application");
+        alertBuilderD.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                switch (which) {
-                    case 0:
-                        InstagramMainActivity instagramMainActivity = mainActivity;
-                        DeveloperOptionsLauncher.loadAndLaunchDeveloperOptions(v.getContext(), instagramMainActivity.All(), mainActivity, instagramMainActivity.A0H);
-                        return;
-                    case 1:
-                        try {
-                            if(Brian.isWriteExternalStoragePermissionGranted(v.getContext())){
-                                Brian.startPermissionDialogActivity(v.getContext());
-                            }else{
-                                File archivoDev = new File(v.getContext().getFilesDir(), "mobileconfig"+File.separator+"mc_overrides.json");
-                                if(archivoDev.exists()){
-                                    File rutaSalida = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getAbsolutePath()+File.separator+"InstaDev"+File.separator+"Developer_Mode_Backups"+File.separator);
-                                    if(!rutaSalida.exists()){
-                                        rutaSalida.mkdirs();
-                                    }
-                                    File archivoSalida = new File(rutaSalida, "BackupDev_"+new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss").format(new Date())+".json");
-                                    if (!archivoSalida.exists()){
-                                        archivoSalida.createNewFile();
-                                    }
-                                    FileInputStream FID = new FileInputStream(archivoDev);
-                                    FileOutputStream FOS = new FileOutputStream(archivoSalida);
-                                    byte[] buf = new byte[1024];
-                                    int len;
-                                    while ((len = FID.read(buf)) > 0) {
-                                        FOS.write(buf, 0, len);
-                                    }
-                                    Brian.showToast(v.getContext(),"File exported in"+ archivoSalida.getPath(), Toast.LENGTH_LONG);
-                                    FID.close();
-                                    FOS.close();
-                                }else{
-                                    Brian.showToast(v.getContext(), "There is no configuration file to export", Toast.LENGTH_LONG);
-                                }
-                            }
-                        }catch (Exception e){
-                            Brian.showToast(v.getContext(),"Error: Could not export developer mode settings", Toast.LENGTH_LONG);
-                        }
-                        return;
-                    case 2:
-                        Intent intent = new Intent(v.getContext(), RestoreBackupDev.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        v.getContext().startActivity(intent);
-                        return;
-                    default:
-                        return;
-                }
+                dialog.dismiss();
             }
         });
-        builder.create();
-        builder.show();
-        return true;
+        alertBuilderD.setPositiveButton("RESTART", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ((AlarmManager) ctx.getSystemService(Context.ALARM_SERVICE)).set(AlarmManager.RTC, 100L + System.currentTimeMillis(), PendingIntent.getActivity(ctx, 123456, ctx.getPackageManager().getLaunchIntentForPackage(ctx.getPackageName()), PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_IMMUTABLE));
+                System.exit(0);
+            }
+        });
+
+        alertBuilderD.create();
+        alertBuilderD.show();
+    }
+
+    public static void onLongClickMenu(final InstagramMainActivity activity, View v) {
+        v.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                String[] strListaMenu = {"Open Developer Menu", "Create developer mode backup", "Restore developer mode backup", "View developer channel"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setItems(strListaMenu, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                DeveloperOptionsLauncher.loadAndLaunchDeveloperOptions(v.getContext(), activity.All(), activity, activity.A0H);
+                                return;
+                            case 1:
+                                try {
+                                    if(Brian.isWriteExternalStoragePermissionGranted(v.getContext())){
+                                        Brian.startPermissionDialogActivity(v.getContext());
+                                    }else{
+                                        File archivoDev = new File(v.getContext().getFilesDir(), "mobileconfig"+File.separator+"mc_overrides.json");
+                                        if(archivoDev.exists()){
+                                            File rutaSalida = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getAbsolutePath()+File.separator+"InstaDev"+File.separator+"Developer_Mode_Backups"+File.separator);
+                                            if(!rutaSalida.exists()){
+                                                rutaSalida.mkdirs();
+                                            }
+                                            File archivoSalida = new File(rutaSalida, "BackupDev_"+new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss").format(new Date())+".json");
+                                            if (!archivoSalida.exists()){
+                                                archivoSalida.createNewFile();
+                                            }
+                                            FileInputStream FID = new FileInputStream(archivoDev);
+                                            FileOutputStream FOS = new FileOutputStream(archivoSalida);
+                                            byte[] buf = new byte[1024];
+                                            int len;
+                                            while ((len = FID.read(buf)) > 0) {
+                                                FOS.write(buf, 0, len);
+                                            }
+                                            Brian.showToast(v.getContext(),"File exported in"+ archivoSalida.getPath(), Toast.LENGTH_LONG);
+                                            FID.close();
+                                            FOS.close();
+                                        }else{
+                                            Brian.showToast(v.getContext(), "There is no configuration file to export", Toast.LENGTH_LONG);
+                                        }
+                                    }
+                                }catch (Exception e){
+                                    Brian.showToast(v.getContext(),"Error: Could not export developer mode settings", Toast.LENGTH_LONG);
+                                }
+                                return;
+                            case 2:
+                                Intent intent = new Intent(v.getContext(), RestoreBackupDev.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                v.getContext().startActivity(intent);
+                                return;
+                            case 3:
+                                String url = "https://t.me/bsewamods";
+                                Uri uri = Uri.parse(url);
+                                Intent inte = new Intent(Intent.ACTION_VIEW, uri);
+                                v.getContext().startActivity(inte);
+                                return;
+                            default:
+                                return;
+                        }
+                    }
+                });
+                builder.create();
+                builder.show();
+                return true;
+            }
+        });
     }
 }
 
